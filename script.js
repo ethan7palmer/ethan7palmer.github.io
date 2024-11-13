33033885105dd1ccaa8f2f4644dc580f4500085b
Ethan Palmer <ethan7palmer@gmail.com>
2024-11-13 16:37:35 -0600

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let body = document.querySelector("body");
 
-body.addEventListener("click", function (event) {
+body.addEventListener("click", (e) => {
   let spark = document.createElement("div");
   spark.classList.add("spark");
   body.appendChild(spark);
@@ -9,7 +9,7 @@ body.addEventListener("click", function (event) {
   spark.style.left = event.clientX - body.offsetLeft + "px";
   spark.style.filter = "hue-rotate(" + Math.random() * 360 + "deg)";
 
-  for (var i = 0; i <= 7; i++) {
+  for (let i = 0; i <= 7; i++) {
     let span = document.createElement("span");
     span.style.transform = "rotate(" + i * 45 + "deg)";
     spark.appendChild(span);
@@ -18,4 +18,30 @@ body.addEventListener("click", function (event) {
   setTimeout(function () {
     spark.remove();
   }, 1000);
+});
+
+// time since last added trail element
+let lastTrailTime = 0;
+// delay in milliseconds between trail
+const trailDelay = 20;
+
+document.addEventListener("mousemove", (e) => {
+  const currentTime = Date.now();
+
+  if (currentTime - lastTrailTime > trailDelay) {
+    lastTrailTime = currentTime;
+
+    let element = document.createElement("div");
+    element.setAttribute("class", "trail");
+    element.setAttribute(
+      "style",
+      `left: ${e.clientX - 5}px; top: ${e.clientY - 5}px;`
+    );
+
+    element.onanimationend = () => {
+      element.remove();
+    };
+
+    body.insertAdjacentElement("beforeend", element);
+  }
 });
